d7db695007b4b51dcbb1284c1ce6d2daf5a9e0ca
Viktor <67437+vfonic@users.noreply.github.com>
2025-01-08 13:05:33 -0300

    Get to failing test

--- tasks/bug_report_template.rb
+++ tasks/bug_report_template.rb
@@ -20,6 +20,10 @@ gemfile(true) do
   gem "sqlite3", force_ruby_platform: true, platform: :mri
   gem "activerecord-jdbcsqlite3-adapter", platform: :jruby
 
+  gem "activerecord-session_store"
+  gem "devise"
+  gem "pundit"
+
   # Fixes an issue on CI with default gems when using inline bundle with default
   # gems that are already activated
   # Ref: rubygems/rubygems#6386
@@ -33,6 +37,22 @@ gemfile(true) do
 end
 
 require "active_record"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "devise"
+require "devise/orm/active_record"
+require "active_admin"
+
+# Add silence method to Logger
+class Logger
+  def silence(severity = Logger::ERROR)
+    old_level = level
+    self.level = severity
+    yield
+  ensure
+    self.level = old_level
+  end
+end
 
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 ActiveRecord::Base.logger = Logger.new(STDOUT)
@@ -43,17 +63,33 @@ ActiveRecord::Schema.define do
 
   create_table :users, force: true do |t|
     t.string :full_name
+    t.integer :manager_of_forum_id
+    t.string :email
+    t.string :encrypted_password
+    t.string   :reset_password_token
+    t.datetime :reset_password_sent_at
+    t.datetime :remember_created_at
+    t.timestamps
+  end
+
+  create_table :forums, force: true do |t|
+    t.string :name
+  end
+
+  create_table :forum_threads, force: true do |t|
+    t.string :title
+    t.integer :forum_id
   end
 end
-
-require "action_controller/railtie"
-require "action_view/railtie"
-require "active_admin"
 
 class TestApp < Rails::Application
   config.root = __dir__
   config.hosts << ".example.com"
-  config.session_store :cookie_store, key: "cookie_store_key"
+
+  # Use ActiveRecord session store instead of cookie store
+  config.session_store :active_record_store,
+    key: "cookie_store_key"
+
   config.secret_key_base = "secret_key_base"
   config.eager_load = false
 
@@ -78,12 +114,94 @@ class ApplicationRecord < ActiveRecord::Base
 end
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
+class Forum < ApplicationRecord
+  has_many :forum_threads
+end
+
+class ForumThread < ApplicationRecord
+  belongs_to :forum
+end
+
+Devise.setup do |config|
+  # config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 12
+  # config.reconfirmable = true
+  # config.expire_all_remember_me_on_sign_out = true
+  # config.password_length = 6..128
+  # config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # config.reset_password_within = 6.hours
+  # config.sign_out_via = :delete
 end
 
 ActiveAdmin.setup do |config|
+  config.comments = false
+  config.authorization_adapter = ActiveAdmin::PunditAdapter
   # Authentication disabled by default. Override if necessary.
-  config.authentication_method = false
-  config.current_user_method = false
+  # config.authentication_method = false
+  config.authentication_method = :authenticate_user!
+  # config.current_user_method = false
+  config.current_user_method = :current_user
+
+  # Add these lines to configure logout behavior
+  config.logout_link_path = :destroy_user_session_path
+  # config.logout_link_method = :delete
+end
+
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  # def scope = Pundit.policy_scope!(user, record.class)
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve = scope
+  end
+end
+
+class ActiveAdmin::PagePolicy < ApplicationPolicy
+  def show? = true
+end
+
+class UserPolicy < ApplicationPolicy
+  def index? = true
+
+  class Scope < ::ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+end
+
+class ForumPolicy < ApplicationPolicy
+  def index? = true
+
+  class Scope < ::ApplicationPolicy::Scope
+    def resolve
+      scope.all # where(id: user.manager_of_forum_id)
+    end
+  end
+end
+
+class ForumThreadPolicy < ApplicationPolicy
+  def index? = true
 end
 
 Rails.application.initialize!
@@ -98,7 +216,14 @@ end
 ActiveAdmin.register User do
 end
 
+ActiveAdmin.register Forum do
+end
+
+ActiveAdmin.register ForumThread do
+end
+
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
 
@@ -108,19 +233,21 @@ require "rails/test_help"
 
 # Replace this with the code necessary to make your test fail.
 class BugTest < ActionDispatch::IntegrationTest
+  include ::Warden::Test::Helpers
+  Warden.test_mode!
 
-  def test_admin_root_success?
-    get admin_root_url
-    assert_match "Test Me", response.body # has content
-    assert_match "Users", response.body # has 'Your Models' in menu
-    assert_response :success
-  end
+  teardown { Warden.test_reset! }
 
-  def test_admin_users
-    User.create! full_name: "John Doe"
-    get admin_users_url
-    assert_match "John Doe", response.body # has created row
+  def test_admin_cannot_create_forum_thread_for_another_forum
+    admin_forum = Forum.create! name: "Test Forum"
+    other_forum = Forum.create! name: "Other Forum"
+    user = User.create! email: "test@example.com", password: "password", full_name: "John Doe", manager_of_forum_id: admin_forum.id
+    login_as user, scope: :user
+
+    get admin_forums_url
+
     assert_response :success
+    assert_no_match other_forum.name, response.body
   end
 
   private
